d03d8f35e98a2616a34c1efbb773726fef579dc9
mariekebrinkman <44111394+mariekebrinkman@users.noreply.github.com>
2019-11-05 19:12:32 +0100

    eerste versie van de website

---
+++ js/script.js
@@ -0,0 +1,8 @@
+ function verander1() {
+    //hide foto 1
+    document.getElementById("foto1").style.display= "none";
+    //show foto 2
+    document.getElementById("foto2").style.display="block";
+}
+
+//en dat dan voor elke foto
